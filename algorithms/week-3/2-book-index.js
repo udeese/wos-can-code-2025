4258a22d5968cb82a5da23536b870fcfcd4b4549
narcisolobo <narcisolobo@gmail.com>
2025-07-21 07:04:14 -0700

    w3d1 algos

---
+++ algorithms/week-3/2-book-index.js
@@ -0,0 +1,29 @@
+/*
+Book Index
+
+Given an array of ints representing page numbers
+return a string with the page numbers formatted as page ranges when the nums
+span a consecutive range.
+*/
+
+const numsA = [1, 13, 14, 15, 37, 38, 70];
+const expectedA = '1, 13-15, 37-38, 70';
+
+const numsB = [5, 6, 7, 8, 9];
+const expectedB = '5-9';
+
+const numsC = [1, 2, 3, 7, 9, 15, 16, 17];
+const expectedC = '1-3, 7, 9, 15-17';
+
+/**
+ * Turns the given arr of page numbers into a string of comma hyphenated
+ * page ranges.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums Page numbers.
+ * @returns {string} The given page numbers as comma separated hyphenated
+ *    page ranges.
+ */
+function bookIndex(nums) {
+  //your code here
+}
