22d4930e8a2db6e9a9ac351769c513942ee7f5b8
narcisolobo <narcisolobo@gmail.com>
2025-08-11 12:29:02 -0700

    w6d1 afternoon

--- 4-intro-to-react/9-moviedex/src/components/MovieForm.jsx
+++ 4-intro-to-react/9-moviedex/src/components/MovieForm.jsx
@@ -1,43 +1,113 @@
 import { useState } from 'react';
 
 const blankMovie = { title: '', year: '' };
-const errors = { title: '', year: '' };
+const blankErrors = { title: '', year: '' };
 
-function MovieForm() {
+function MovieForm({ onAddMovie }) {
   const [movie, setMovie] = useState(blankMovie);
+  const [errors, setErrors] = useState(blankErrors);
+
+  const isFormValid = errors.title === '' && errors.year === '';
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMovie((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleBlur = (e) => {
+    const { name, value } = e.target;
+    validateFields(name, value);
+  };
+
+  const validateFields = (name, value) => {
+    let message = '';
+    switch (name) {
+      case 'title':
+        if (value.trim().length === 0) {
+          message = 'Title is required.';
+        }
+        if (value.trim().length < 2) {
+          message = 'Title must be at least 2 characters.';
+        }
+        break;
+      case 'year':
+        if (value.trim().length === 0) {
+          message = 'Year is required.';
+        }
+        if (isNaN(value) || value < 1888 || value > 2030) {
+          message = 'Invalid year.';
+        }
+        break;
+      default:
+        break;
+    }
+    setErrors((prev) => ({ ...prev, [name]: message }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    Object.entries(movie).forEach(([name, value]) => {
+      console.log(name, value);
+      validateFields(name, value);
+    });
+
+    if (!errors.title && !errors.year) {
+      const newMovie = { ...movie, id: crypto.randomUUID(), isWatched: false };
+      onAddMovie(newMovie);
+
+      setMovie(blankMovie);
+      setErrors(blankErrors);
+      console.log(movie);
+    }
+  };
 
   return (
     <div className="card shadow mb-3">
       <div className="card-body">
-        <div className="d-flex justify-content-between align-items-center gap-2">
-          <div className="flex-grow-1">
-            <label htmlFor="title" className="visually-hidden">
-              Title
-            </label>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              className="form-control"
-              placeholder="Title:"
-            />
+        <form onSubmit={handleSubmit}>
+          <div className="d-flex justify-content-between align-items-start gap-2">
+            <div className="flex-grow-1">
+              <label htmlFor="title" className="visually-hidden">
+                Title
+              </label>
+              <input
+                value={movie.title}
+                type="text"
+                name="title"
+                id="title"
+                className="form-control"
+                placeholder="Title:"
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
+              {errors.title && (
+                <span className="form-text text-warning">{errors.title}</span>
+              )}
+            </div>
+            <div className="flex-grow-1">
+              <label htmlFor="year" className="visually-hidden">
+                Year
+              </label>
+              <input
+                value={movie.year}
+                type="number"
+                name="year"
+                id="year"
+                className="form-control"
+                placeholder="Year:"
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
+              {errors.year && (
+                <span className="form-text text-warning">{errors.year}</span>
+              )}
+            </div>
+            <button type="submit" className="btn btn-primary text-nowrap">
+              Add Movie
+            </button>
           </div>
-          <div className="flex-grow-1">
-            <label htmlFor="year" className="visually-hidden">
-              Year
-            </label>
-            <input
-              type="number"
-              name="year"
-              id="year"
-              className="form-control"
-              placeholder="Year:"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary text-nowrap">
-            Add Movie
-          </button>
-        </div>
+        </form>
       </div>
     </div>
   );
--- 4-intro-to-react/9-moviedex/src/components/MoviedexApp.jsx
+++ 4-intro-to-react/9-moviedex/src/components/MoviedexApp.jsx
@@ -6,15 +6,40 @@ import MovieForm from './MovieForm';
 function MoviedexApp() {
   const [movies, setMovies] = useState([]);
 
+  const handleAddMovie = (newMovie) => {
+    setMovies((prev) => [newMovie, ...prev]);
+  };
+
+  const handleToggleWatched = (targetId) => {
+    const toggledMovies = movies.map((movie) => {
+      if (movie.id === targetId) {
+        return { ...movie, isWatched: !movie.isWatched };
+      }
+      return movie;
+    });
+
+    setMovies(toggledMovies);
+  };
+
+  const handleDelete = (targetId) => {
+    const filteredMovies = movies.filter((movie) => movie.id !== targetId);
+    setMovies(filteredMovies);
+  };
+
   return (
     <Fragment>
-      <MovieForm />
+      <MovieForm onAddMovie={handleAddMovie} />
       {movies.length === 0 ? (
         <p>Your Moviedex is empty! Add a movie above.</p>
       ) : (
         <div className={styles['card-grid']}>
           {movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              onDelete={handleDelete}
+              onToggleWatched={handleToggleWatched}
+            />
           ))}
         </div>
       )}
