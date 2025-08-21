17d0cb4ce81d2796f8fe95e4f7ba8a68f0e86ff7
narcisolobo <narcisolobo@gmail.com>
2025-08-21 09:21:41 -0700

    w7d4 morning

---
+++ 5-dotnet/week-1/4-thursday/1-oop/OOPCarApp/Classes/Car.cs
@@ -0,0 +1,61 @@
+using System.Threading.Tasks.Dataflow;
+
+namespace OOPCarApp.Classes;
+
+class Car
+{
+    // private string _make; created automagically
+    public string? Make { get; set; } // now a property
+    public string Model { get; set; } // now a property
+    public string Color { get; set; } // now a property
+    private int _year; // backing field
+
+    public string MakeModel // read only property
+    {
+        get { return $"{Make} {Model}"; }
+    }
+
+    public int Year
+    {
+        get { return _year; }
+        set
+        {
+            if (value > 1885 && value < DateTime.Now.Year + 1)
+            {
+                _year = value;
+            }
+            else
+            {
+                Console.WriteLine("Invalid year value. Setting to 1885.");
+                _year = 1885;
+            }
+        }
+    }
+
+    public Car()
+    {
+        Make = "Unknown";
+        Model = "Unknown";
+        Year = 0;
+        Color = "Unknown";
+    }
+
+    public Car(string make, string model, int year, string color)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        Color = color;
+    }
+
+    // Instance Method (Behavior)
+    public void StartEngine()
+    {
+        Console.WriteLine($"{Color} {Make} {Model}'s engine is starting!");
+    }
+
+    public void Drive(int distance)
+    {
+        Console.WriteLine($"The {Make} {Model} is driving {distance} miles.");
+    }
+}
---
+++ 5-dotnet/week-1/4-thursday/1-oop/OOPCarApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using OOPCarApp.Classes;
+
+class Program
+{
+    static void Main()
+    {
+        var myCar = new Car("Jeep", "Wrangler", 2021, "Blue");
+        // {
+        //     Make = "Jeep",
+        //     Model = "Wrangler",
+        //     Year = 2021,
+        //     Color = "Blue",
+        // };
+
+        Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model} ({myCar.Color}).");
+
+        myCar.StartEngine();
+        myCar.Drive(100);
+
+        var unknownCar = new Car();
+        Console.WriteLine(
+            $"My car is a {unknownCar.Year} {unknownCar.Make} {unknownCar.Model} ({unknownCar.Color})."
+        );
+
+        var invalidYearCar = new Car("Jeep", "Wrangler", 1884, "Blue");
+        Console.WriteLine(
+            $"My car is a {invalidYearCar.Year} {invalidYearCar.Make} {invalidYearCar.Model} ({invalidYearCar.Color})."
+        );
+
+        Console.WriteLine(myCar.MakeModel);
+    }
+}
