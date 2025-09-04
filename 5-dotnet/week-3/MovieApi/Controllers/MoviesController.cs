67092960570c4a25fb13adb94536d48fa3282aec
narcisolobo <narcisolobo@gmail.com>
2025-09-03 22:42:33 -0700

    w9d2 algo prep

--- 5-dotnet/week-3/MovieApi/Controllers/MoviesController.cs
+++ 5-dotnet/week-3/MovieApi/Controllers/MoviesController.cs
@@ -37,4 +37,30 @@ public class MoviesController : ControllerBase
         }
         return Ok(maybeMovie);
     }
+
+    [HttpGet("search")]
+    public ActionResult<List<Movie>> Search(string? keyword, double? minRating)
+    {
+        var query = _movies.AsQueryable();
+
+        if (!string.IsNullOrEmpty(keyword))
+        {
+            query = query.Where(
+                (movie) => movie.Title.Contains(keyword, StringComparison.OrdinalIgnoreCase)
+            );
+        }
+
+        if (minRating.HasValue)
+        {
+            query = query.Where(m => m.Rating >= minRating.Value);
+        }
+
+        var results = query.ToList();
+
+        if (results.Count == 0)
+        {
+            return NotFound("No movies found matching the search criteria.");
+        }
+        return Ok(results);
+    }
 }
