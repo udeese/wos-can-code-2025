6df57066d654133134799eb1f7e5f3b9cbd4e372
Ursula Deese <u.deese@yahoo.com>
2025-09-11 09:00:29 -0500

    /

--- 5-dotnet/week-1/2-tuesday/RockPaperScissors/obj/Debug/net8.0/RockPaperScissors.AssemblyInfo.cs
+++ 5-dotnet/week-1/2-tuesday/RockPaperScissors/obj/Debug/net8.0/RockPaperScissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68126b8e052760edd17234daf01e20a8bb2852d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115e052e00b56e7c20360f8042461e63a53ef5e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
