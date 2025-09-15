c2477a53e589e94ff4ca2a72e71069e5a3ea6772
narcisolobo <narcisolobo@gmail.com>
2025-09-15 07:18:32 -0700

    w11d1 algo prep

---
+++ 0-algorithms/week-11/hash-tables/index.js
@@ -0,0 +1,13 @@
+function simpleHash(key, size) {
+  let total = 0;
+  for (let i = 0; i < key.length; i++) {
+    total += key.charCodeAt(i);
+    console.log(`character: ${key[i]}, charCode: ${key.charCodeAt(i)}`);
+  }
+  console.log(`Total: ${total}, hash: ${total % size}`);
+  return total % size;
+}
+
+// Example usage:
+const index = simpleHash('dog', 10); // Might return 4
+console.log(index);
