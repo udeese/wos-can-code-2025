cc84da7a15deaca210b0730e4aaeb19e171e703d
narcisolobo <narcisolobo@gmail.com>
2025-09-18 07:08:21 -0700

    w11d4 algo prep

--- 0-algorithms/week-11/hash-tables/index.js
+++ 0-algorithms/week-11/hash-tables/index.js
@@ -1,13 +1,15 @@
-function simpleHash(key, size) {
-  let total = 0;
-  for (let i = 0; i < key.length; i++) {
-    total += key.charCodeAt(i);
-    console.log(`character: ${key[i]}, charCode: ${key.charCodeAt(i)}`);
-  }
-  console.log(`Total: ${total}, hash: ${total % size}`);
-  return total % size;
-}
+// function simpleHash(key, size) {
+//   let total = 0;
+//   for (let i = 0; i < key.length; i++) {
+//     total += key.charCodeAt(i);
+//     console.log(`character: ${key[i]}, charCode: ${key.charCodeAt(i)}`);
+//   }
+//   console.log(`Total: ${total}, hash: ${total % size}`);
+//   return total % size;
+// }
 
-// Example usage:
-const index = simpleHash('dog', 10); // Might return 4
-console.log(index);
+// // Example usage:
+// const index = simpleHash('dog', 10); // Might return 4
+// console.log(index);
+
+console.log(typeof '1');
