293831a687150b6d2fa989ed204ef276561595ba
narcisolobo <narcisolobo@gmail.com>
2025-10-06 08:10:04 -0700

    w14d1 algo

--- 0-algorithms/leet-code/6-longest-common-subsequence/index.js
+++ 0-algorithms/leet-code/6-longest-common-subsequence/index.js
@@ -10,14 +10,22 @@
  * @returns {number}
  */
 function longestCommonSubsequence(text1, text2) {
-  // TODO: Implement bottom-up DP
-  // 1) Let m = text1.length, n = text2.length
-  // 2) Create (m+1) x (n+1) array dp filled with 0
-  // 3) For i from 1..m:
-  //      For j from 1..n:
-  //        If text1[i-1] === text2[j-1]: dp[i][j] = dp[i-1][j-1] + 1
-  //        Else: dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
-  // 4) Return dp[m][n]
+  let m = text1.length;
+  let n = text2.length;
+  if (m === 0 || n === 0) return 0;
+
+  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[m][n];
 }
 
 export { longestCommonSubsequence };
