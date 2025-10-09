4469be5c49fb51917ef736f708f1db31dcc6af50
narcisolobo <narcisolobo@gmail.com>
2025-10-09 07:10:28 -0700

    w14d4 algo prep

--- 0-algorithms/leet-code/9-integer-to-roman/index.js
+++ 0-algorithms/leet-code/9-integer-to-roman/index.js
@@ -10,6 +10,22 @@
 function intToRoman(num) {
   // TODO: Convert integer to Roman numeral
   // 1. Create two parallel arrays of Roman symbols and their numeric values, ordered from largest to smallest.
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const symbols = [
+    'M',
+    'CM',
+    'D',
+    'CD',
+    'C',
+    'XC',
+    'L',
+    'XL',
+    'X',
+    'IX',
+    'V',
+    'IV',
+    'I',
+  ];
   // 2. Start with an empty result string.
   // 3. While the number is greater than zero:
   //   - Find the largest value less than or equal to the number.
