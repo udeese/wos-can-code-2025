57842eded28105f00409705d69ea28df1d2e26d5
Ursula Deese <u.deese@yahoo.com>
2025-10-10 09:02:02 -0500

    //

--- 0-algorithms/leet-code/9-integer-to-roman/index.js
+++ 0-algorithms/leet-code/9-integer-to-roman/index.js
@@ -1,3 +1,50 @@
+// /**
+//  * Converts an integer to a Roman numeral.
+//  *
+//  * Time: O(1)
+//  * Space: O(1)
+//  *
+//  * @param {number} num - Integer between 1 and 3999.
+//  * @returns {string} Roman numeral string.
+//  */
+// function intToRoman(num) {
+//   // TODO: Convert integer to Roman numeral
+//   // 1. Create two parallel arrays of Roman symbols and their numeric values, ordered from largest to smallest.
+//   const values = [
+//     1000, 900, 500, 400,
+//     100, 90, 50, 40,
+//     10, 9, 5, 4,
+//     1
+//   ];
+
+//   const symbols = [
+//     "M", "CM", "D", "CD",
+//     "C", "XC", "L", "XL",
+//     "X", "IX", "V", "IV",
+//     "I"
+//   ];
+//   // 2. Start with an empty result string.
+//   let result = "";
+//   // 3. While the number is greater than zero:
+//   while (num > 0) {
+//     //   - Find the largest value less than or equal to the number.
+//     for (let i = 0; i < values.length; i++) {
+//       if (num >= values[i]) {
+//         result += symbols[i];
+//         num -= values[i];
+//         break;
+//       }
+//     }
+//   }
+//   return result;
+//   //   - Append its symbol to the result.
+//   //   - Subtract that value from the number.
+//   // 4. Return the result string.
+// }
+
+// export { intToRoman };
+
+
 /**
  * Converts an integer to a Roman numeral.
  *
@@ -8,14 +55,30 @@
  * @returns {string} Roman numeral string.
  */
 function intToRoman(num) {
-  // TODO: Convert integer to Roman numeral
-  // 1. Create two parallel arrays of Roman symbols and their numeric values, ordered from largest to smallest.
-  // 2. Start with an empty result string.
-  // 3. While the number is greater than zero:
-  //   - Find the largest value less than or equal to the number.
-  //   - Append its symbol to the result.
-  //   - Subtract that value from the number.
-  // 4. Return the result string.
+  const values = [
+    1000, 900, 500, 400,
+    100, 90, 50, 40,
+    10, 9, 5, 4,
+    1
+  ];
+  
+  const symbols = [
+    "M", "CM", "D", "CD",
+    "C", "XC", "L", "XL",
+    "X", "IX", "V", "IV",
+    "I"
+  ];
+
+  let result = "";
+
+  for (let i = 0; i < values.length; i++) {
+    while (num >= values[i]) {
+      result += symbols[i];
+      num -= values[i];
+    }
+  }
+
+  return result;
 }
 
 export { intToRoman };
--- 5-dotnet/week-1/2-tuesday/RockPaperScissors/obj/Debug/net8.0/RockPaperScissors.AssemblyInfo.cs
+++ 5-dotnet/week-1/2-tuesday/RockPaperScissors/obj/Debug/net8.0/RockPaperScissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fe10c58d363caef34e583b9d9b6372cdabeeec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c836b182d3b76c66ee53784840135a6c68b2a2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
